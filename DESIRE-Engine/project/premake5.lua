53fea94cb2ae0dc749d1a9dbbf5ea0fd1fda8cfc
nyaki <nyakacska.zoltan@gmail.com>
2017-04-05 20:08:57 +0200

    Updated projects

--- DESIRE-Engine/project/premake5.lua
+++ DESIRE-Engine/project/premake5.lua
@@ -1,26 +1,17 @@
 project "DESIRE"
-	kind "StaticLib"
-	location(_ACTION)
-	objdir(_ACTION .. "/obj/%{cfg.buildcfg}/")
-	targetdir("../lib/" .. _ACTION)
-
+	AddCommonLibConfig()
 	uuid "74923478-6D8D-4892-A013-C2B759A870EC"
 
 	includedirs
 	{
-		"../src",
 		"../../../DESIRE-modules",
 		"../../../DESIRE-modules/Physics-Bullet/src",
 		"../../../DESIRE-modules/Render-bgfx/include",
 		"../../../DESIRE-modules/ResourceLoader-Assimp/include",
 	}
 
-	files
-	{
-		"../src/**.h",
-		"../src/**.hpp",
-		"../src/**.cpp",
-	}
+	pchheader "stdafx.h"
+	pchsource "stdafx.cpp"
 
 	filter "system:not android"
 		removefiles
--- premake5.lua
+++ premake5.lua
@@ -5,6 +5,7 @@ characterset "MBCS"
 floatingpoint "Fast"
 rtti "Off"
 editandcontinue "Off"
+warnings "Extra"
 
 buildoptions { "/Gw", "/Zc:rvalueCast" }
 flags { "StaticRuntime", "MultiProcessorCompile", "C++11" }
@@ -12,6 +13,31 @@ flags { "StaticRuntime", "MultiProcessorCompile", "C++11" }
 if _ACTION == "vs2017" then
 	systemversion "10.0.10240.0"
 	defines { "WIN32", "_HAS_EXCEPTIONS=0" }
+end
+
+function AddCommonLibConfig()
+	kind "StaticLib"
+	location(_ACTION)
+	objdir(_ACTION .. "/obj/%{cfg.buildcfg}/")
+	targetdir("../lib/" .. _ACTION)
+
+	defines { "_LIB" }
+
+	includedirs
+	{
+		"../include",
+		"../src",
+	}
+
+	files
+	{
+		"../include/**.h",
+		"../include/**.hpp",
+		"../src/**.h",
+		"../src/**.hpp",
+		"../src/**.c",
+		"../src/**.cpp",
+	}
 end
 
 configuration "Debug"
