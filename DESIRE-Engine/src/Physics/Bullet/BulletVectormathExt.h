43fc3bcaa820b9b473d141dfb527916425f2dee1
nyaki <nyakacska.zoltan@gmail.com>
2017-11-01 21:35:55 +0100

    Use serialize methods for converting

--- DESIRE-Engine/src/Physics/Bullet/BulletVectormathExt.h
+++ DESIRE-Engine/src/Physics/Bullet/BulletVectormathExt.h
@@ -6,43 +6,63 @@
 #include "Physics-Bullet/src/LinearMath/btQuaternion.h"
 
 #if (defined(DESIRE_USE_SSE) && defined(BT_USE_SSE)) || (defined(__ARM_NEON__) && defined(BT_USE_NEON))
-	#define BULLET_VECTORMATH_USE_VEC128	1
-#else
-	#define BULLET_VECTORMATH_USE_VEC128	0
+	#define USE_NATIVE_CONVERSION
 #endif
 
 inline Vector3 GetVector3(const btVector3& bulletVec)
 {
-#if BULLET_VECTORMATH_USE_VEC128
+#if defined(USE_NATIVE_CONVERSION)
 	return Vector3(bulletVec.get128());
 #else
-	return Vector3((float)bulletVec.getX(), (float)bulletVec.getY(), (float)bulletVec.getZ());
+	btVector3FloatData data;
+	bulletVec.serializeFloat(data);
+
+	Vector3 vec;
+	vec.LoadXYZ(data.m_floats);
+	return vec;
 #endif
 }
 
 inline btVector3 GetBtVector3(const Vector3& vec)
 {
-#if BULLET_VECTORMATH_USE_VEC128
+#if defined(USE_NATIVE_CONVERSION)
 	return btVector3(vec);
 #else
-	return btVector3(vec.GetX(), vec.GetY(), vec.GetZ());
+	btVector3FloatData data = {};
+	vec.StoreXYZ(data.m_floats);
+
+	btVector3 bulletVec;
+	bulletVec.deSerializeFloat(data);
+	return bulletVec;
 #endif
 }
 
 inline Quat GetQuat(const btQuaternion& bulletQuat)
 {
-#if BULLET_VECTORMATH_USE_VEC128
+#if defined(USE_NATIVE_CONVERSION)
 	return Quat(bulletQuat.get128());
 #else
-	return Quat((float)bulletQuat.getX(), (float)bulletQuat.getY(), (float)bulletQuat.getZ(), (float)bulletQuat.getW());
+	btQuaternionFloatData data;
+	bulletQuat.serializeFloat(data);
+
+	Quat quat;
+	quat.LoadXYZW(data.m_floats);
+	return quat;
 #endif
 }
 
 inline btQuaternion GetBtQuat(const Quat& quat)
 {
-#if BULLET_VECTORMATH_USE_VEC128
+#if defined(USE_NATIVE_CONVERSION)
 	return btQuaternion(quat);
 #else
-	return btQuaternion(quat.GetX(), quat.GetY(), quat.GetZ(), quat.GetW());
+	btQuaternionFloatData data;
+	quat.StoreXYZW(data.m_floats);
+
+	btQuaternion bulletQuat;
+	bulletQuat.deSerializeFloat(data);
+	return bulletQuat;
 #endif
 }
+
+#undef USE_NATIVE_CONVERSION
