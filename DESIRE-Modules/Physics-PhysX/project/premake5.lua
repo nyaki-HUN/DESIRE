206dc4ce90881f66da57edbe9b89953d44a4b609
nyaki <nyakacska.zoltan@gmail.com>
2017-12-15 21:58:46 +0100

    Regenerated project

--- DESIRE-Modules/Physics-PhysX/project/premake5.lua
+++ DESIRE-Modules/Physics-PhysX/project/premake5.lua
@@ -2,5 +2,10 @@ solution "Physics-PhysX"
 	location(_ACTION)
 
 	project "Physics-PhysX"
-		AddCommonLibConfig()
+		AddModuleConfig()
 		uuid "A7BB4030-6C2C-45AB-9C46-F1C3A6ACFB0F"
+
+		includedirs
+		{
+			"../Externals/PhysX/include",
+		}
