a683d3819ac5ae8be3b04ee1c357b988b5f28456
nyaki <nyakacska.zoltan@gmail.com>
2017-12-15 21:49:12 +0100

    Moved module from old location

---
+++ DESIRE-Modules/Script-AngelScript/project/premake5.lua
@@ -0,0 +1,26 @@
+solution "Script-AngelScript"
+	location(_ACTION)
+
+	project "Script-AngelScript"
+		AddModuleConfig()
+		warnings "Extra"
+		uuid "AE7FBF83-FF16-4E32-9844-6CFB79B22929"
+
+		includedirs
+		{
+			"../Externals/AngelScript/include",
+		}
+
+		filter "system:windows"
+			files
+			{
+				"../Externals/AngelScript/src/as_callfunc_x64_msvc_asm.asm"
+			}
+
+		filter { "system:windows", "files:../Externals/AngelScript/src/**.asm" }
+			buildcommands "ml64.exe /c  /nologo /Fo%{cfg.objdir}/%{file.basename}.obj /W3 /Zi /Ta %{file.relpath}"
+			buildoutputs "%{cfg.objdir}/%{file.basename}.obj;%(Outputs)"
+		filter {}
+
+		configuration "Debug"
+			defines { "AS_DEBUG" }
