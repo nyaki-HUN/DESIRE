c6913b662ad3e932834dab168baff4187f0bdac3
nyaki <nyakacska.zoltan@gmail.com>
2017-12-15 20:43:58 +0100

    Module moved from the old location

---
+++ DESIRE-Modules/Physics-Bullet/project/premake5.lua
@@ -0,0 +1,11 @@
+solution "Physics-Bullet"
+	location(_ACTION)
+
+	project "Physics-Bullet"
+		AddCommonLibConfig()
+		uuid "F4BCB217-F30B-4DE4-9F3C-F1B2013FA45C"
+
+		includedirs
+		{
+			"../Externals/Bullet/src",
+		}
