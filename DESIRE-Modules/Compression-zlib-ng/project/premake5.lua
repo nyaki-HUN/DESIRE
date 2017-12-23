d24a888dfe1c2e2f8d26ce9c785ec9c2f7b22efa
nyaki <nyakacska.zoltan@gmail.com>
2017-12-23 21:12:07 +0100

    Added zlib-ng as submodule

--- DESIRE-Modules/Compression-zlib-ng/project/premake5.lua
+++ DESIRE-Modules/Compression-zlib-ng/project/premake5.lua
@@ -2,15 +2,28 @@ project "Compression-zlib-ng"
 	AddModuleConfig()
 	uuid "07C8AFBA-5E54-4A5B-BD68-C5C8ED113D57"
 
-	defines { "Z_SOLO" }
+	defines
+	{
+		"Z_SOLO",
+		"X86_SSE2_FILL_WINDOW",
+		"X86_CPUID",
+	}
 
 	includedirs
 	{
-		"../Externals/zlib-ng/include",
+		"../Externals/zlib-ng",
 	}
 
 	files
 	{
-		"../Externals/**.h",
-		"../Externals/**.c",
+		"../Externals/zlib-ng/*.h",
+		"../Externals/zlib-ng/*.c",
+		"../Externals/zlib-ng/arch/x86/*.h",
+		"../Externals/zlib-ng/arch/x86/*.c",
+	}
+
+	removefiles
+	{
+		"../Externals/zlib-ng/gz*.h",
+		"../Externals/zlib-ng/gz*.c",
 	}
--- DESIRE-Modules/ResourceLoader-Assimp/project/premake5.lua
+++ DESIRE-Modules/ResourceLoader-Assimp/project/premake5.lua
@@ -3,14 +3,21 @@ project "ResourceLoader-Assimp"
 	uuid "19E05838-5A9C-421F-9102-F3F5A1ED488A"
 
 	rtti "On"
-	defines { "OPENDDL_STATIC_LIBARY", "ASSIMP_BUILD_NO_OWN_ZLIB", "_SCL_SECURE_NO_WARNINGS" }
+
+	defines
+	{
+		"OPENDDL_STATIC_LIBARY",
+		"ASSIMP_BUILD_NO_OWN_ZLIB",
+		"_SCL_SECURE_NO_WARNINGS",
+	}
 
 	includedirs
 	{
 		"../Externals/Assimp/include",
 		"../Externals/Assimp/contrib",
 		"../Externals/Assimp/contrib/openddlparser/include",
-		"../../Compression-zlib-ng/Externals/zlib-ng/include",
+		"../../Compression-zlib-ng/Externals/zlib-ng",
+		"../../Compression-zlib-ng/include",
 	}
 
 	files
