acfdff6a9790fb5ae78f1ae3f45e21624ed76cf5
nyaki <nyakacska.zoltan@gmail.com>
2017-12-23 13:01:34 +0100

    Added empty constructor and destructor for interface class

---
+++ DESIRE-Engine/src/Physics/IPhysics.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Physics/IPhysics.h"
+
+IPhysics::IPhysics()
+{
+
+}
+
+IPhysics::~IPhysics()
+{
+
+}
---
+++ DESIRE-Engine/src/Sound/ISound.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Sound/ISound.h"
+
+ISound::ISound()
+{
+
+}
+
+ISound::~ISound()
+{
+
+}
