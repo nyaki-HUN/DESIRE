56003d5cc3208019fb186b22acb6355e0df270fa
nyaki <nyakacska.zoltan@gmail.com>
2019-03-15 11:56:55 +0100

    Follow the official HLSL naming conventions

--- SandBox/data/shaders/hlsl/fs_ocornut_imgui.hlsl
+++ SandBox/data/shaders/hlsl/fs_ocornut_imgui.hlsl
@@ -3,12 +3,12 @@ Texture2D tex : register(t0);
 
 struct v2f
 {
-	float4 pos : SV_POSITION;
+	float4 pos : SV_Position;
 	float2 uv : TEXCOORD0;
 	float4 color : TEXCOORD1;
 };
 
-float4 main(v2f In) : SV_TARGET
+float4 main(v2f In) : SV_Target
 {
 	return tex.Sample(samplerState, In.uv) * In.color;
 }
