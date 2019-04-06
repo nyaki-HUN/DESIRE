247bc6dd8a1a2eb01cdecf61f09f2ae908c6b59f
nyaki <nyakacska.zoltan@gmail.com>
2019-04-06 12:25:07 +0200

    Moved out old Array implementation to ArrayAsVector.inl until the NEW_ARRAY will be enabled.

--- DESIRE-Engine/src/Engine/Core/Container/Array.h
+++ DESIRE-Engine/src/Engine/Core/Container/Array.h
@@ -44,12 +44,12 @@ public:
 	T& GetAt(size_t idx)
 	{
 		ASSERT(idx < size);
-		return vector[idx];
+		return data[idx];
 	}
 	const T& GetAt(size_t idx) const
 	{
 		ASSERT(idx < size);
-		return vector[idx];
+		return data[idx];
 	}
 
 	T& GetFirst()
@@ -80,10 +80,10 @@ public:
 	}
 
 	// Iterators for supporting range-based for loop
-	T* begin()					{ return &data[0]; }
-	const T* begin() const		{ return &data[0]; }
-	T* end()					{ return &data[size]; }
-	const T* end() const		{ return &data[size]; }
+	T* begin()					{ return data; }
+	const T* begin() const		{ return data; }
+	T* end()					{ return data + size; }
+	const T* end() const		{ return data + size; }
 
 	bool IsEmpty() const
 	{
@@ -119,8 +119,8 @@ public:
 	template<class... Args>
 	T& EmplaceAdd(Args&&... args)
 	{
-		new (&data[0]) T(std::forward<Args>(args)...);
-		return data[0];
+//		new (&data[size]) T(std::forward<Args>(args)...);
+//		return data[size++];
 	}
 
 	void Insert(size_t pos, const T& value)
@@ -188,7 +188,13 @@ public:
 
 	void RemoveLast()
 	{
+		if(size == 0)
+		{
+			return;
+		}
 
+		DestructElements(data + size - 1, 1);
+		size--;
 	}
 
 	void Swap(Array& other)
@@ -214,116 +220,6 @@ private:
 
 #else
 
-#include <vector>
-
-template<typename T>
-class Array
-{
-public:
-	Array()											{}
-	Array(const Array& otherArray)					: vector(otherArray.vector) {}
-	Array(Array&& otherArray)						: vector(std::move(otherArray.vector)) {}
-	Array(std::initializer_list<T> initList)		: vector(initList) {}
-
-	Array& operator =(const Array& otherArray)		{ vector = otherArray.vector; return *this; }
-	Array& operator =(Array&& otherArray)			{ vector = std::move(otherArray.vector); return *this; }
-
-	T& operator [](size_t idx)						{ return vector[idx]; }
-	const T& operator [](size_t idx) const			{ return vector[idx]; }
-	T& GetAt(size_t idx)							{ return vector.at(idx); }
-	const T& GetAt(size_t idx) const				{ return vector.at(idx); }
-	T& GetFirst()									{ return vector.front(); }
-	const T& GetFirst() const						{ return vector.front(); }
-	T& GetLast()									{ return vector.back(); }
-	const T& GetLast() const						{ return vector.back(); }
-
-	T* Data() const									{ return vector.data(); }
-
-	// Iterators for supporting range-based for loop
-	T* begin()										{ return &(*vector.begin()); }
-	const T* begin() const							{ return &(*vector.begin()); }
-	T* end()										{ return &(*vector.end()); }
-	const T* end() const							{ return &(*vector.end()); }
-
-	bool IsEmpty() const							{ return vector.empty(); }
-	size_t Size() const								{ return vector.size(); }
-	void Reserve(size_t numElements)				{ vector.reserve(numElements); }
-	void Clear()									{ vector.clear(); }
-
-	void Add(const T& value)						{ vector.push_back(value); }
-	void Add(T&& value)								{ vector.push_back(std::move(value)); }
-
-	template<class... Args>
-	T& EmplaceAdd(Args&&... args)					{ return vector.emplace_back(std::forward<Args>(args)...); }
-
-	void Insert(size_t pos, const T& value)			{ vector.insert(value); }
-	void Insert(size_t pos, T&& value)				{ vector.insert(std::move(value)); }
-
-	size_t Find(const T& value) const
-	{
-		for(size_t i = 0; i < vector.size(); ++i)
-		{
-			if(vector[i] == value)
-			{
-				return i;
-			}
-		}
-
-		return SIZE_MAX;
-	}
-
-	size_t SpecializedFind(std::function<bool(const T&)> compareFunc) const
-	{
-		for(size_t i = 0; i < vector.size(); ++i)
-		{
-			if(compareFunc(vector[i]))
-			{
-				return i;
-			}
-		}
-
-		return SIZE_MAX;
-	}
-
-	// Do a binary search to find the index of a given element
-	size_t BinaryFind(const T& value) const
-	{
-		auto it = std::lower_bound(vector.begin(), vector.end(), value);
-		return (it != vector.end() && !(value < *it)) ? (it - vector.begin()) : SIZE_MAX;
-	}
-
-	// Do a binary search to find an element by value or insert it into a sorted array 
-	T& BinaryFindOrInsert(T&& value)
-	{
-		auto it = std::lower_bound(vector.begin(), vector.end(), value);
-		return (it != vector.end() && !(value < *it)) ? *it : *vector.insert(it, std::move(value));
-	}
-
-	T& BinaryFindOrInsert(T&& value, bool(*compareFunc)(const T&, const T&))
-	{
-		auto it = std::lower_bound(vector.begin(), vector.end(), value, compareFunc);
-		return (it != vector.end() && !compareFunc(value, *it)) ? *it : *vector.insert(it, std::move(value));
-	}
-
-	bool Remove(const T& value)
-	{
-		const size_t idx = Find(value);
-		if(idx != SIZE_MAX)
-		{
-			Remove(idx);
-			return true;
-		}
-
-		return false;
-	}
-
-	void Remove(size_t idx)							{ vector.erase(vector.begin() + idx); }
-	void RemoveLast()								{ vector.pop_back(); }
-
-	void Swap(Array& other)							{ vector.swap(other.vector); }
-
-private:
-	std::vector<T> vector;
-};
+#include "Engine/Core/Container/ArrayAsVector.inl"
 
 #endif
---
+++ DESIRE-Engine/src/Engine/Core/Container/ArrayAsVector.inl
@@ -0,0 +1,208 @@
+#include <vector>
+
+template<typename T>
+class Array
+{
+public:
+	Array()
+	{
+
+	}
+
+	Array(const Array& otherArray)
+		: vector(otherArray.vector)
+	{
+
+	}
+
+	Array(Array&& otherArray)
+		: vector(std::move(otherArray.vector))
+	{
+
+	}
+
+	Array(std::initializer_list<T> initList)
+		: vector(initList)
+	{
+
+	}
+
+	Array& operator =(const Array& otherArray)
+	{
+		vector = otherArray.vector; return *this;
+	}
+	Array& operator =(Array&& otherArray)
+	{
+		vector = std::move(otherArray.vector); return *this;
+	}
+
+	T& operator [](size_t idx)
+	{
+		return vector[idx];
+	}
+	const T& operator [](size_t idx) const
+	{
+		return vector[idx];
+	}
+
+	T& GetAt(size_t idx)
+	{
+		return vector[idx];
+	}
+	const T& GetAt(size_t idx) const
+	{
+		return vector[idx];
+	}
+
+	T& GetFirst()
+	{
+		return vector.front();
+	}
+	const T& GetFirst() const
+	{
+		return vector.front();
+	}
+
+	T& GetLast()
+	{
+		return vector.back();
+	}
+	const T& GetLast() const
+	{
+		return vector.back();
+	}
+
+	T* Data() const
+	{
+		return vector.data();
+	}
+
+	// Iterators for supporting range-based for loop
+	T* begin()					{ return vector.data(); }
+	const T* begin() const		{ return vector.data(); }
+	T* end()					{ return vector.data() + vector.size(); }
+	const T* end() const		{ return vector.data() + vector.size(); }
+
+	bool IsEmpty() const
+	{
+		return vector.empty();
+	}
+
+	size_t Size() const
+	{
+		return vector.size();
+	}
+
+	void Reserve(size_t numElements)
+	{
+		vector.reserve(numElements);
+	}
+
+	// Erases all elements from the Array, but doesn't free any memory
+	void Clear()
+	{
+		vector.clear();
+	}
+
+	void Add(const T& value)
+	{
+		vector.push_back(value);
+	}
+
+	void Add(T&& value)
+	{
+		vector.push_back(std::move(value));
+	}
+
+	template<class... Args>
+	T& EmplaceAdd(Args&&... args)
+	{
+		return vector.emplace_back(std::forward<Args>(args)...);
+	}
+
+	void Insert(size_t pos, const T& value)
+	{
+		vector.insert(value);
+	}
+
+	void Insert(size_t pos, T&& value)
+	{
+		vector.insert(std::move(value));
+	}
+
+	size_t Find(const T& value) const
+	{
+		for(size_t i = 0; i < vector.size(); ++i)
+		{
+			if(vector[i] == value)
+			{
+				return i;
+			}
+		}
+
+		return SIZE_MAX;
+	}
+
+	size_t SpecializedFind(std::function<bool(const T&)> compareFunc) const
+	{
+		for(size_t i = 0; i < vector.size(); ++i)
+		{
+			if(compareFunc(vector[i]))
+			{
+				return i;
+			}
+		}
+
+		return SIZE_MAX;
+	}
+
+	// Do a binary search to find the index of a given element
+	size_t BinaryFind(const T& value) const
+	{
+		auto it = std::lower_bound(vector.begin(), vector.end(), value);
+		return (it != vector.end() && !(value < *it)) ? (it - vector.begin()) : SIZE_MAX;
+	}
+
+	// Do a binary search to find an element by value or insert it into a sorted array 
+	T& BinaryFindOrInsert(T&& value)
+	{
+		auto it = std::lower_bound(vector.begin(), vector.end(), value);
+		return (it != vector.end() && !(value < *it)) ? *it : *vector.insert(it, std::move(value));
+	}
+
+	T& BinaryFindOrInsert(T&& value, bool(*compareFunc)(const T&, const T&))
+	{
+		auto it = std::lower_bound(vector.begin(), vector.end(), value, compareFunc);
+		return (it != vector.end() && !compareFunc(value, *it)) ? *it : *vector.insert(it, std::move(value));
+	}
+
+	bool Remove(const T& value)
+	{
+		const size_t idx = Find(value);
+		if(idx != SIZE_MAX)
+		{
+			Remove(idx);
+			return true;
+		}
+
+		return false;
+	}
+
+	void Remove(size_t idx)
+	{
+		vector.erase(vector.begin() + idx);
+	}
+
+	void RemoveLast()
+	{
+		vector.pop_back();
+	}
+
+	void Swap(Array& other)
+	{
+		vector.swap(other.vector);
+	}
+
+private:
+	std::vector<T> vector;
+};
