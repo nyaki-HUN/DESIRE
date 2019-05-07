85007e0bfce9c324f0df6a93ec6cba1683fc4e7a
nyaki <nyakacska.zoltan@gmail.com>
2019-05-08 01:27:13 +0200

    Cleanup

--- DESIRE-Engine/src/Engine/Application/OSWindow_windows.cpp
+++ DESIRE-Engine/src/Engine/Application/OSWindow_windows.cpp
@@ -13,128 +13,126 @@
 class OSWindowImpl
 {
 public:
-	static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+	{
+		if(msg == WM_CREATE)
+		{
+			// Store pointer to the WINDOWSWindow in user data area
+			CREATESTRUCT *createStruct = (CREATESTRUCT*)lParam;
+			SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)createStruct->lpCreateParams);
+			return 0;
+		}
+
+		// Get the window instance
+		OSWindow *window = reinterpret_cast<OSWindow*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+		if(window == nullptr)
+		{
+			return DefWindowProc(hWnd, msg, wParam, lParam);
+		}
+
+		if(window->isActive)
+		{
+			auto it = window->additionalMessageHandlers.find(msg);
+			if(it != window->additionalMessageHandlers.end())
+			{
+				it->second(&wParam, &lParam);
+			}
+		}
+
+		switch(msg)
+		{
+			case WM_MOUSEMOVE:			// Posted to a window when the cursor moves
+				return 0;
+
+			case WM_CHAR:				// Posted to the window when a WM_KEYDOWN message is translated by TranslateMessage()
+				return 0;
+
+			case WM_SYSKEYDOWN:
+			case WM_SYSKEYUP:
+				// Alt + F4 should be enabled to close the window
+				if(wParam == VK_F4 && lParam & 1 << 29)
+				{
+					break;
+				}
+				// Do not process the system keys since they would activate the context menu of the window or the main menu
+				return 0;
+
+/*			case WM_SETCURSOR:
+				SetCursor(nullptr);
+				return 1;
+*/
+			case WM_ACTIVATE:			// Sent to both the window being activated and the window being deactivated
+				if(wParam == WA_INACTIVE)
+				{
+					window->isActive = false;
+					Modules::Application->SendEvent(EAppEventType::EnterBackground);
+				}
+				else if(wParam == WA_ACTIVE || wParam == WA_CLICKACTIVE)
+				{
+					window->isActive = true;
+					Modules::Application->SendEvent(EAppEventType::EnterForeground);
+				}
+				break;
+
+			case WM_ENTERSIZEMOVE:		// Sent one time to a window after it enters the moving or sizing modal loop
+				window->impl->isInSizeMove = true;
+				return 0;
+
+			case WM_EXITSIZEMOVE:		// Sent one time to a window, after it has exited the moving or sizing modal loop
+			{
+				window->impl->isInSizeMove = false;
+
+				WINDOWINFO pwi;
+				GetWindowInfo(hWnd, &pwi);
+				window->SetSize((uint16_t)(pwi.rcClient.right - pwi.rcClient.left), (uint16_t)(pwi.rcClient.bottom - pwi.rcClient.top));
+				return 0;
+			}
+
+			case WM_SIZE:				// Sent to a window after its size has changed
+				if(!window->impl->isInSizeMove && wParam != SIZE_MINIMIZED)
+				{
+					if(wParam == SIZE_MAXIMIZED)
+					{
+//						window->SetPosition(0, 0);
+					}
+					window->SetSize((uint16_t)lParam, (uint16_t)(lParam >> 16));
+				}
+				return 0;
+
+			case WM_DISPLAYCHANGE:		// Sent to all windows when the display resolution has changed
+				break;
+
+			case WM_GETMINMAXINFO:		// When the size or position of the window is about to change
+				// Prevent the window from going smaller than some minimum size
+				((MINMAXINFO*)lParam)->ptMinTrackSize.x = OSWindow::kWindowMinSize;
+				((MINMAXINFO*)lParam)->ptMinTrackSize.y = OSWindow::kWindowMinSize;
+				break;
+
+			case WM_SYSCOMMAND:
+				switch(wParam)
+				{
+					case SC_MOVE:
+					case SC_SIZE:
+					case SC_KEYMENU:
+					case SC_SCREENSAVE:
+					case SC_MONITORPOWER:
+						return 0;
+				}
+				break;
+
+			case WM_QUIT:				// Generated when the application calls the PostQuitMessage() function
+			case WM_CLOSE:				// Sent as a signal that a window or an application should terminate
+				Application::Stop();
+				return 0;
+		}
+
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
 
 	HWND hWnd = 0;
 	HCURSOR cursors[OSWindow::NUM_CURSORS] = {};
 	bool isInSizeMove = false;
 };
-
-LRESULT CALLBACK OSWindowImpl::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	if(msg == WM_CREATE)
-	{
-		// Store pointer to the WINDOWSWindow in user data area
-		CREATESTRUCT *createStruct = (CREATESTRUCT*)lParam;
-		SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)createStruct->lpCreateParams);
-		return 0;
-	}
-
-	// Get the window instance
-	OSWindow *window = reinterpret_cast<OSWindow*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
-	if(window == nullptr)
-	{
-		return DefWindowProc(hWnd, msg, wParam, lParam);
-	}
-
-	if(window->isActive)
-	{
-		auto it = window->additionalMessageHandlers.find(msg);
-		if(it != window->additionalMessageHandlers.end())
-		{
-			it->second(&wParam, &lParam);
-		}
-	}
-
-	switch(msg)
-	{
-		case WM_MOUSEMOVE:			// Posted to a window when the cursor moves
-			return 0;
-
-		case WM_CHAR:				// Posted to the window when a WM_KEYDOWN message is translated by TranslateMessage()
-			return 0;
-
-		case WM_SYSKEYDOWN:
-		case WM_SYSKEYUP:
-			// Alt + F4 should be enabled to close the window
-			if(wParam == VK_F4 && lParam & 1 << 29)
-			{
-				break;
-			}
-			// Do not process the system keys since they would activate the context menu of the window or the main menu
-			return 0;
-
-/*		case WM_SETCURSOR:
-			SetCursor(nullptr);
-			return 1;
-*/
-		case WM_ACTIVATE:			// Sent to both the window being activated and the window being deactivated
-			if(wParam == WA_INACTIVE)
-			{
-				window->isActive = false;
-				Modules::Application->SendEvent(EAppEventType::EnterBackground);
-			}
-			else if(wParam == WA_ACTIVE || wParam == WA_CLICKACTIVE)
-			{
-				window->isActive = true;
-				Modules::Application->SendEvent(EAppEventType::EnterForeground);
-			}
-			break;
-
-		case WM_ENTERSIZEMOVE:		// Sent one time to a window after it enters the moving or sizing modal loop
-			window->impl->isInSizeMove = true;
-			return 0;
-
-		case WM_EXITSIZEMOVE:		// Sent one time to a window, after it has exited the moving or sizing modal loop
-		{
-			window->impl->isInSizeMove = false;
-
-			WINDOWINFO pwi;
-			GetWindowInfo(hWnd, &pwi);
-			window->SetSize((uint16_t)(pwi.rcClient.right - pwi.rcClient.left), (uint16_t)(pwi.rcClient.bottom - pwi.rcClient.top));
-			return 0;
-		}
-
-		case WM_SIZE:				// Sent to a window after its size has changed
-			if(!window->impl->isInSizeMove && wParam != SIZE_MINIMIZED)
-			{
-				if(wParam == SIZE_MAXIMIZED)
-				{
-//					window->SetPosition(0, 0);
-				}
-				window->SetSize((uint16_t)lParam, (uint16_t)(lParam >> 16));
-			}
-			return 0;
-
-		case WM_DISPLAYCHANGE:		// Sent to all windows when the display resolution has changed
-			break;
-
-		case WM_GETMINMAXINFO:		// When the size or position of the window is about to change
-			// Prevent the window from going smaller than some minimum size
-			((MINMAXINFO*)lParam)->ptMinTrackSize.x = OSWindow::kWindowMinSize;
-			((MINMAXINFO*)lParam)->ptMinTrackSize.y = OSWindow::kWindowMinSize;
-			break;
-
-		case WM_SYSCOMMAND:
-			switch(wParam)
-			{
-				case SC_MOVE:
-				case SC_SIZE:
-				case SC_KEYMENU:
-				case SC_SCREENSAVE:
-				case SC_MONITORPOWER:
-					return 0;
-			}
-			break;
-
-		case WM_QUIT:				// Generated when the application calls the PostQuitMessage() function
-		case WM_CLOSE:				// Sent as a signal that a window or an application should terminate
-			Application::Stop();
-			return 0;
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 // --------------------------------------------------------------------------------------------------------------------
 //	OSWindow
