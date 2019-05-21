d224ef0ac404bf253e6e3cca3131f531759adc48
nyaki <nyakacska.zoltan@gmail.com>
2019-05-21 21:01:11 +0200

    Removed Modules.cpp and put the module definitions into Application.cpp

--- DESIRE-Engine/src/Engine/Application/Application.cpp
+++ DESIRE-Engine/src/Engine/Application/Application.cpp
@@ -11,6 +11,15 @@
 #include "Engine/Script/ScriptSystem.h"
 #include "Engine/Sound/SoundSystem.h"
 
+// Module definitions
+std::unique_ptr<Application> Modules::Application;
+std::unique_ptr<Input> Modules::Input;
+std::unique_ptr<Physics> Modules::Physics;
+std::unique_ptr<Render> Modules::Render;
+std::unique_ptr<ResourceManager> Modules::ResourceManager;
+std::unique_ptr<ScriptSystem> Modules::ScriptSystem;
+std::unique_ptr<SoundSystem> Modules::SoundSystem;
+
 bool Application::s_isMainLoopRunning = false;
 int Application::s_returnValue = 0;
 
