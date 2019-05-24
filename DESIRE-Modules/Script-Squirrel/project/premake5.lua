4c77cdf784d20022fb780b1a59336d746f84f095
nyaki <nyakacska.zoltan@gmail.com>
2019-05-24 21:34:45 +0200

    Regenerated projects

--- DESIRE-Modules/Script-Squirrel/project/premake5.lua
+++ DESIRE-Modules/Script-Squirrel/project/premake5.lua
@@ -2,6 +2,8 @@ project "Script-Squirrel"
 	AddModuleConfig()
 	uuid "05885A51-71F1-4606-A989-2AA3C69EEA91"
 
+	defines { "SCRAT_USE_CXX11_OPTIMIZATIONS" }
+
 	includedirs
 	{
 		"../Externals/squirrel/include",
