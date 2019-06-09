6e93ccbfee355454e8ff397c8c66017517fab0cc
nyaki <nyakacska.zoltan@gmail.com>
2019-06-09 11:46:55 +0200

    Renamed folder "memory" to "Memory"

--- UnitTest/src/Core/Memory_/test_StackAllocator.cpp
+++ UnitTest/src/Core/Memory_/test_StackAllocator.cpp
@@ -5,19 +5,31 @@ TEST_CASE("StackAllocator", "[Core][memory]")
 {
 	StackAllocator<128> a;
 
-	const void *memoryStart = (char*)&a + sizeof(LinearAllocator);
-	const void *memoryEnd = (char*)memoryStart + (sizeof(a) - sizeof(LinearAllocator));
+	const void* memoryStart = (char*)& a + sizeof(LinearAllocator);
+	const void* memoryEnd = (char*)memoryStart + (sizeof(a) - sizeof(LinearAllocator));
+
+	SECTION("Default alignment")
+	{
+		const size_t numAllocBegin = globalMemoryAllocationCount;
+
+		void* ptr1 = a.Allocate(1);
+		void* ptr2 = a.Allocate(1);
+		CHECK_NO_ALLOCATION_SINCE(numAllocBegin);
+
+		CHECK(reinterpret_cast<uintptr_t>(ptr1) % Allocator::kDefaultAlignment == 0);
+		CHECK(reinterpret_cast<uintptr_t>(ptr2) % Allocator::kDefaultAlignment == 0);
+	}
 
 	SECTION("Test Fallback Allocator 1/4")
 	{
 		const size_t numAllocBegin = globalMemoryAllocationCount;
 
 		// ptr3 is supposed to be allocated via the fallback allocator
-		void *ptr1 = a.Allocate(64);
-		void *ptr2 = a.Allocate(64);
+		void* ptr1 = a.Allocate(64);
+		void* ptr2 = a.Allocate(64);
 		CHECK_NO_ALLOCATION_SINCE(numAllocBegin);
-		void *ptrFB = a.Allocate(10);
-		
+		void* ptrFB = a.Allocate(10);
+
 		CHECK((memoryStart <= ptr1 && ptr1 < memoryEnd));
 		CHECK((memoryStart <= ptr2 && ptr2 < memoryEnd));
 		CHECK_FALSE((memoryStart <= ptrFB && ptrFB < memoryEnd));
@@ -31,12 +43,12 @@ TEST_CASE("StackAllocator", "[Core][memory]")
 	{
 		size_t numAllocBegin = globalMemoryAllocationCount;
 
-		void *ptr1 = a.Allocate(64);
+		void* ptr1 = a.Allocate(64);
 		CHECK_NO_ALLOCATION_SINCE(numAllocBegin);
-		void *ptrFB = a.Allocate(100);
+		void* ptrFB = a.Allocate(100);
 
 		numAllocBegin = globalMemoryAllocationCount;
-		void *ptr2 = a.Allocate(10);
+		void* ptr2 = a.Allocate(10);
 		CHECK_NO_ALLOCATION_SINCE(numAllocBegin);
 
 		CHECK((memoryStart <= ptr1 && ptr1 < memoryEnd));
@@ -50,7 +62,7 @@ TEST_CASE("StackAllocator", "[Core][memory]")
 
 	SECTION("Test Fallback Allocator 3/4")
 	{
-		void *ptrFB = a.Allocate(200);
+		void* ptrFB = a.Allocate(200);
 		CHECK_FALSE((memoryStart <= ptrFB && ptrFB < memoryEnd));
 
 		a.Deallocate(ptrFB);
@@ -59,8 +71,8 @@ TEST_CASE("StackAllocator", "[Core][memory]")
 	SECTION("Test Fallback Allocator 4/4")
 	{
 		// Because of the alignment the second allocations cannot be made regardless of the fact that the overall size would be only 128
-		void *ptr1 = a.Allocate(113);
-		void *ptrFB = a.Allocate(15);
+		void* ptr1 = a.Allocate(113);
+		void* ptrFB = a.Allocate(15);
 		CHECK((memoryStart <= ptr1 && ptr1 < memoryEnd));
 		CHECK_FALSE((memoryStart <= ptrFB && ptrFB < memoryEnd));
 
