0831a1ee2f6444f1dd0ef16ebe7a0bcd17255222
nyaki <nyakacska.zoltan@gmail.com>
2020-01-11 00:25:53 +0100

    Improved compile and link  times.

--- DESIRE-Modules/Script-Lua/src/API/CoreAPI_Lua.cpp
+++ DESIRE-Modules/Script-Lua/src/API/CoreAPI_Lua.cpp
@@ -10,31 +10,28 @@
 void RegisterCoreAPI_Lua(sol::state_view& lua)
 {
 	// Component
-	lua.new_usertype<Component>("Component",
-		"enabled", sol::property(&Component::IsEnabled, &Component::SetEnabled),
-		"object", sol::property(&Component::GetObject)
-	);
+	auto component = lua.new_usertype<Component>("Component");
+	component.set("enabled", sol::property(&Component::IsEnabled, &Component::SetEnabled));
+	component.set("object", sol::property(&Component::GetObject));
 
 	// Object
-	lua.new_usertype<Object>("Object",
-		"GetObjectName", &Object::GetObjectName,
-		"SetActive", &Object::SetActive,
-		"IsActiveSelf", &Object::IsActiveSelf,
-		"IsActiveInHierarchy", &Object::IsActiveInHierarchy,
-		"RemoveComponent", &Object::RemoveComponent,
-		"GetComponent", &Object::GetComponentByTypeId,
-		"transform", sol::property(&Object::GetTransform),
-		"parent", sol::property(&Object::GetParent, &Object::SetParent)
-	);
+	auto object = lua.new_usertype<Object>("Object");
+	object.set_function("GetObjectName", &Object::GetObjectName);
+	object.set_function("SetActive", &Object::SetActive);
+	object.set_function("IsActiveSelf", &Object::IsActiveSelf);
+	object.set_function("IsActiveInHierarchy", &Object::IsActiveInHierarchy);
+	object.set_function("RemoveComponent", &Object::RemoveComponent);
+	object.set_function("GetComponent", &Object::GetComponentByTypeId);
+	object.set("transform", sol::property(&Object::GetTransform));
+	object.set("parent", sol::property(&Object::GetParent, &Object::SetParent));
 
 	// Timer
-	lua.new_usertype<Timer>("ITimer",
-		"GetTimeMicroSec", &Timer::GetTimeMicroSec,
-		"GetTimeMilliSec", &Timer::GetTimeMilliSec,
-		"GetTimeSec", &Timer::GetTimeSec,
-		"GetMicroDelta", &Timer::GetMicroDelta,
-		"GetMilliDelta", &Timer::GetMilliDelta,
-		"GetSecDelta", &Timer::GetSecDelta);
-
-	lua.set("Timer", Modules::Application->GetTimer());
+	auto timer = lua.new_usertype<Timer>("ITimer");
+	timer.set_function("GetTimeMicroSec", &Timer::GetTimeMicroSec);
+	timer.set_function("GetTimeMilliSec", &Timer::GetTimeMilliSec);
+	timer.set_function("GetTimeSec", &Timer::GetTimeSec);
+	timer.set_function("GetMicroDelta", &Timer::GetMicroDelta);
+	timer.set_function("GetMilliDelta", &Timer::GetMilliDelta);
+	timer.set_function("GetSecDelta", &Timer::GetSecDelta);
+	lua["Timer"] = Modules::Application->GetTimer();
 }
