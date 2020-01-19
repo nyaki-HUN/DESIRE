846243e0dffdd6b55fed969e6df463601f3ffa0f
nyaki <nyakacska.zoltan@gmail.com>
2020-01-19 20:38:32 +0100

    Fixed build issues

--- DESIRE-Modules/Network-curl/project/premake5.lua
+++ DESIRE-Modules/Network-curl/project/premake5.lua
@@ -2,9 +2,19 @@ project "Network-curl"
 	AddModuleConfig()
 	uuid "B908992D-EAFC-4E3E-9D31-651D2A8EBA3D"
 
+	removeflags { "FatalCompileWarnings" }
+
+	defines
+	{
+		"BUILDING_LIBCURL",
+		"CURL_STATICLIB",
+		"HTTP_ONLY",
+	}
+
 	includedirs
 	{
-		"../Externals/curl",
+		"../Externals/curl/include",
+		"../Externals/curl/src",
 	}
 
 	files
