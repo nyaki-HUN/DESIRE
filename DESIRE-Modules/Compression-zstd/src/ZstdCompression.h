b497c8d7810cda66ad208a2e91a4531716beb9f3
nyaki <nyakacska.zoltan@gmail.com>
2020-01-24 21:19:48 +0100

    Cleanup

--- DESIRE-Modules/Compression-zstd/src/ZstdCompression.h
+++ DESIRE-Modules/Compression-zstd/src/ZstdCompression.h
@@ -12,17 +12,17 @@ public:
 	~ZstdCompression();
 
 	size_t GetMaxCompressionDataBufferSize(size_t dataSize) const override;
-	size_t CompressBuffer(void *compressedDataBuffer, size_t compressedDataBufferSize, const void *data, size_t dataSize) override;
+	size_t CompressBuffer(void* compressedDataBuffer, size_t compressedDataBufferSize, const void* data, size_t dataSize) override;
 
-	size_t GetMaxDecompressionDataBufferSize(const void *compressedData, size_t compressedDataSize) const override;
-	size_t DecompressBuffer(void *dataBuffer, size_t dataBufferSize, const void *compressedData, size_t compressedDataSize) override;
+	size_t GetMaxDecompressionDataBufferSize(const void* compressedData, size_t compressedDataSize) const override;
+	size_t DecompressBuffer(void* dataBuffer, size_t dataBufferSize, const void* compressedData, size_t compressedDataSize) override;
 
 	int GetMinCompressionLevel() const override;
 	int GetMaxCompressionLevel() const override;
 
 private:
-	static void* MallocWrapper(void *opaque, size_t size);
-	static void FreeWrapper(void *opaque, void *address);
+	static void* MallocWrapper(void* opaque, size_t size);
+	static void FreeWrapper(void* opaque, void* address);
 
 	ZSTD_CCtx* compressContext = nullptr;
 	ZSTD_DCtx* decompressContext = nullptr;
